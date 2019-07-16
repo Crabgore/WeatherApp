7913385fee9ab22e19c13857b1596d5cbab6075d
Mihail Dorofeev <crabgore.gonna.hate@gmail.com>
2019-07-16 19:34:55 +0200

    Android. Базовый уровень. Практчиеское задание №3

--- app/src/main/java/com/geekbrains/android_1/weatherapp/MainActivity.java
+++ app/src/main/java/com/geekbrains/android_1/weatherapp/MainActivity.java
@@ -1,109 +1,79 @@
 package com.geekbrains.android_1.weatherapp;
 
-import android.os.PersistableBundle;
+import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.RadioButton;
+import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String RADIOGROUP_1_CHECKED_KEY = "is_radiogroup_1_checked";
-    private static final String RADIOGROUP_2_CHECKED_KEY = "is_radiogroup_2_checked";
-    private RadioButton c;
-    private RadioButton f;
-    private RadioButton kmh;
-    private RadioButton ms;
+    TextView city;
+    TextView windSpeed;
+    TextView windSpeedValue;
+    TextView windSpeedUnits;
+    TextView pressure;
+    TextView pressureValue;
+    TextView pressureUnits;
+    TextView temperature;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.settings);
-        Toast.makeText(getApplicationContext(), "Приложение создано", Toast.LENGTH_SHORT).show();
-        Log.d("LifeCycle","Приложение создано" );
+        setContentView(R.layout.activity_main);
 
-        c = findViewById(R.id.C);
-        f = findViewById(R.id.F);
-        kmh = findViewById(R.id.kmh);
-        ms = findViewById(R.id.ms);
+        city = findViewById(R.id.city);
+        windSpeed = findViewById(R.id.windSpeed);
+        windSpeedValue = findViewById(R.id.windSpeed_value);
+        windSpeedUnits = findViewById(R.id.windSpeed_units);
+        pressure = findViewById(R.id.pressure);
+        pressureValue = findViewById(R.id.pressure_value);
+        pressureUnits = findViewById(R.id.pressure_units);
+        temperature = findViewById(R.id.temperature);
 
+    }
+
+    public void city_choose(View view) {
+        Intent intent = new Intent(MainActivity.this, CityChooser.class);
+        startActivityForResult(intent, 1);
+    }
+
+    public void settings(View view) {
+        Intent intent = new Intent(MainActivity.this, Settings.class);
+        startActivityForResult(intent, 2);
     }
 
     @Override
-    protected void onStart() {
-        super.onStart();
-        Toast.makeText(getApplicationContext(), "Приложение запущено", Toast.LENGTH_SHORT).show();
-        Log.d("LifeCycle","Приложение запущено" );
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Toast.makeText(getApplicationContext(), "Приложение остановлено", Toast.LENGTH_SHORT).show();
-        Log.d("LifeCycle","Приложение остановлено" );
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Toast.makeText(getApplicationContext(), "Приложение закрыто", Toast.LENGTH_SHORT).show();
-        Log.d("LifeCycle","Приложение закрыто" );
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Toast.makeText(getApplicationContext(), "Приложение приостановлено", Toast.LENGTH_SHORT).show();
-        Log.d("LifeCycle","Приложение приостановлено" );
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Toast.makeText(getApplicationContext(), "Приложение восстановлено", Toast.LENGTH_SHORT).show();
-        Log.d("LifeCycle","Приложение восстановлено" );
-    }
-
-    @Override
-    public void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        Toast.makeText(getApplicationContext(), "Данные приложения сохранены", Toast.LENGTH_SHORT).show();
-        Log.d("LifeCycle","Данные приложения сохранены" );
-        if (c.isChecked()) {
-            outState.putString(RADIOGROUP_1_CHECKED_KEY, "c");
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == 1){
+            city.setText(data.getStringExtra("CITY"));
+            if (!data.getExtras().getBoolean("WINDSPEED")){
+                windSpeed.setVisibility(View.INVISIBLE);
+                windSpeedValue.setVisibility(View.INVISIBLE);
+                windSpeedUnits.setVisibility(View.INVISIBLE);
+            } else {
+                windSpeed.setVisibility(View.VISIBLE);
+                windSpeedValue.setVisibility(View.VISIBLE);
+                windSpeedUnits.setVisibility(View.VISIBLE);
+            }
+            if (!data.getExtras().getBoolean("PRESSURE")){
+                pressure.setVisibility(View.INVISIBLE);
+                pressureValue.setVisibility(View.INVISIBLE);
+                pressureUnits.setVisibility(View.INVISIBLE);
+            } else {
+                pressure.setVisibility(View.VISIBLE);
+                pressureValue.setVisibility(View.VISIBLE);
+                pressureUnits.setVisibility(View.VISIBLE);
+            }
         }
-        if (f.isChecked()) {
-            outState.putString(RADIOGROUP_1_CHECKED_KEY, "f");
-        }
-        if (kmh.isChecked()) {
-            outState.putString(RADIOGROUP_2_CHECKED_KEY, "kmh");
-        }
-        if (ms.isChecked()) {
-            outState.putString(RADIOGROUP_2_CHECKED_KEY, "ms");
+        if (requestCode == 2){
+            if (data.getExtras().getString("TMP").equals("c")){
+                temperature.setText("16°c");
+            } else temperature.setText("16°f");
+            if (data.getExtras().getString("SPD").equals("ms")){
+                windSpeedUnits.setText("м/с");
+            } else  windSpeedUnits.setText("км/ч");
         }
     }
-
-    @Override
-    protected void onRestoreInstanceState(Bundle savedInstanceState) {
-        super.onRestoreInstanceState(savedInstanceState);
-        Toast.makeText(getApplicationContext(), "Данные приложения восстановлены", Toast.LENGTH_SHORT).show();
-        Log.d("LifeCycle","Данные приложения восстановлены" );
-        String s1 = savedInstanceState.getString(RADIOGROUP_1_CHECKED_KEY);
-        String s2 = savedInstanceState.getString(RADIOGROUP_1_CHECKED_KEY);
-        if (s1.equals("c")){
-            c.setChecked(true);
-        }
-        if (s1.equals("f")){
-            f.setChecked(true);
-        }
-        if (s2.equals("kmh")){
-            kmh.setChecked(true);
-        }
-        if (s2.equals("ms")){
-            ms.setChecked(true);
-        }
-    }
-
 }
